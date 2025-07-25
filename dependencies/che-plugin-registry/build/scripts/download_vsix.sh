28f4d57976db4ba6b7557a629eee231945ea21a2
Valeriy Svydenko <vsvydenk@redhat.com>
2025-07-25 16:36:58 +0300

    set code version to validate extensions
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- dependencies/che-plugin-registry/build/scripts/download_vsix.sh
+++ dependencies/che-plugin-registry/build/scripts/download_vsix.sh
@@ -92,7 +92,8 @@ getVersions() {
 }
 
 echo "Scripts branch=${scriptBranch}"
-codeVersion=$(curl -sSlko- https://raw.githubusercontent.com/redhat-developer/devspaces-images/"${scriptBranch}"/devspaces-code/code/package.json | jq -r '.version')
+# codeVersion=$(curl -sSlko- https://raw.githubusercontent.com/redhat-developer/devspaces-images/"${scriptBranch}"/devspaces-code/code/package.json | jq -r '.version')
+codeVersion="1.99.3"
 echo "Che Code version=${codeVersion}"
 
 # Check if the information about the current branch is empty
